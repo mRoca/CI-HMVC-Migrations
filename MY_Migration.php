7db97feaf489d56b4bf1ad59538add5eb17dbb1e
Michel Roca <mroca.dh@gmail.com>
2014-11-06 09:29:00 +0100

    Add modules_list and module_exists functions

--- MY_Migration.php
+++ MY_Migration.php
@@ -1,5 +1,43 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+//Modules helpers, you can move this functions to a true helper file
+if (!function_exists('module_exists')) {
+    /**
+     * Return the CodeIgniter modules list
+     * @param bool $with_location
+     * @return array
+     */
+    function modules_list($with_location = TRUE)
+    {
+        !function_exists('directory_map') && get_instance()->load->helper('directory');
+
+        $modules = array();
+
+        foreach (Modules::$locations as $location => $offset) {
+
+            $files = directory_map($location, 1);
+            if (is_array($files)) {
+                foreach ($files as $name) {
+                    if (is_dir($location . $name))
+                        $modules[] = $with_location ? array($location, $name) : $name;
+                }
+            }
+        }
+
+        return $modules;
+    }
+
+    /**
+     * Check if a CodeIgniter module with the given name exists
+     * @param $module_name
+     * @return bool
+     */
+    function module_exists($module_name)
+    {
+        return in_array($module_name, modules_list(FALSE));
+    }
+}
+
 if (!function_exists('normalizePath')) {
 
 	/**
